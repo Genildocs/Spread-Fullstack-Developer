587919b10740972f4a06ef8a993dcc456f6667f0
Genildo <genildocs@gmail.com>
2022-06-14 21:44:53 -0300

    Segundo commit

--- arraypares.js
+++ arraypares.js
@@ -1,9 +1,10 @@
 function substituiPar(array){
     if(!array) return -1;
     if(!array.length) return -1;
+    
 
     for(let i = 0; i < array.length; i++){
-        if(array[i] === 0) {
+        if(array[i] === 0 ) {
             console.log("Você já é zero!")
         } else if (array[i] % 2 === 0){
             console.log(`Substituindo ${array[i]} por zero.`)
@@ -14,4 +15,4 @@ function substituiPar(array){
 }
 
 let lista = [10,5,36,2];
-console.log(substituiPar(lista))
+console.log(substituiPar())
